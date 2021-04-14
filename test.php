578a6d411cc94a3817549e2e9825c1fc7472b6dd
david.propitech <david.propitech@gmail.com>
2021-04-14 14:54:26 +0300

    test php consoleé

---
+++ test.php
@@ -0,0 +1,10 @@
+/* copier ver /usr/local/bin */
+
+$a = (int)readline('Entrez votre note :');
+if($a < 2){
+echo 'kely';
+}else{
+echo 'betska';}
+$text = (string)readline('Entrer votre nom : ');
+echo $text;
+
